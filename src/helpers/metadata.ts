3484c857d9d22ea0adaf461f8586f821652f3aa0
Daan <12269072+D-Un1t@users.noreply.github.com>
2026-06-09 13:50:37 +0200

    Delete src/helpers/metadata.ts

--- src/helpers/metadata.ts
+++
@@ -1,4 +0,0 @@
-export const APP_NAME = "EOSC DataCommons Tool Registry";
-export const APP_DESCRIPTION =
-  "EOSC DataCommons Tool Registry is a platform for discovering and sharing tools related to the EOSC Data Commons.";
-export const APP_KEYWORDS = "EOSC, Data Commons, Tools, Research, Open Science";
